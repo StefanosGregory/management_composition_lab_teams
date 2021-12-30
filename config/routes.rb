933b7b824676264c919bf0400210563fa133d8ec
Stefanos Gregory <gregorystefanos@gmail.com>
2021-12-30 19:56:33 +0200

    register

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {:register => "register"}
   get 'welcome/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+Rails.application.routes.draw do
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+  end 
+end 
+
+
+Rails.application.routes.draw do
+  devise_scope :user do
+    get 'register', to: 'devise/registrations#new'
+  end 
+end 
